59f441817b5867a674d4d899207b119a94718463
alamincoders <developeralamin03@gmail.com>
2023-04-14 22:50:33 +0600

    database added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-export default function Home() {
+import { db } from "@/lib/db";
+
+export default async function Home() {
+  await db.set("hello", "world");
   return (
     <main>
       <h2 className="text-green-500">Hello world</h2>
