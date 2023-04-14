c3cda2e140959a8c9634815b87334085bb61a88d
alamincoders <developeralamin03@gmail.com>
2023-04-14 22:36:17 +0600

    real time chat initialize

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import React from "react";
+
+interface pageProps {}
+
+const Dashboard = ({}) => {
+  return <Button>Hello</Button>;
+};
+
+export default Dashboard;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h2 className="text-green-500">Hello world</h2>
+    </main>
+  );
+}
